67f5411066eb978ddf8d62ef885497f0653e1aff
NamaAnda <hanifahkurniasari0512@gmail.com>
2025-03-13 23:04:36 +0700

    JOBSHEET 4. praktikum 1

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -10,13 +10,12 @@ class UserController extends Controller
 {
     public function index() {
         $data = [
-            'nama' => 'Pelanggan pertama',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
         ];
-        UserModel::where('username', 'customer-1')->update($data); // tambahkan data ke tabel m_user
-            
-            // // coba akses model UserModel
-            // $user = UserModel :: all(); // ambil semua data dari tabel m_user
-            // return view('user', ['data' => $user]);
+         UserModel::create($data);
             
         $user = UserModel::all();
         return view('user', ['data' => $user]);
