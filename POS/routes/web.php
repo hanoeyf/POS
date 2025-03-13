f7164ffe260b28c485b76d2069ec47bd37cb08b3
NamaAnda <hanifahkurniasari0512@gmail.com>
2025-03-13 19:32:08 +0700

    praktikum 2.2 sampai Praktikum 5 Query Builder

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -5,6 +5,15 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\KategoriController;
+// use App\Http\Controllers\LevelController;
+
+Route::get('/', function(){
+    return view('welcome');
+});
+
+// Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 // Halaman Home
 Route::get('/', [HomeController::class, 'index']);
