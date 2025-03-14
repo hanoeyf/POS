43e06b923292794c89649cc459747dd731e017c8
NamaAnda <hanifahkurniasari0512@gmail.com>
2025-03-14 09:46:57 +0700

    praktikum 2.6

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -6,26 +6,50 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
 
     public function index()
     {
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-
-        ]);
-
-        $user->username = 'manager12';
-        $user->save();
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged('username', 'level_id'); // true
-        $user->wasChanged(['nama']);//false
-        dd($user->wasChanged(['nama', 'username'])); // true
-
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
+    public function tambah(){
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request){
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password'=> Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
+    }
+    public function ubah($id){
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+    public function ubah_simpan(Request $request, $id){
+        $user = UserModel::find($id);
+        if (!$user) {
+            return redirect('/user')->with('error', 'User tidak ditemukan');
+        }
+    
+        $user->update([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => $request->password ? Hash::make($request->password) : $user->password,
+            'level_id' => $request->level_id
+        ]);
+    
+        return redirect('/user')->with('success', 'User berhasil diperbarui');
+    }
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
+    }
+    
 }
