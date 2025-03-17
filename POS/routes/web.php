e098288145c28722a90bf82aad39ec435b16d1ed
NamaAnda <hanifahkurniasari0512@gmail.com>
2025-03-17 22:34:02 +0700

    PRAKTIKUM 2

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\LevelController;
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
@@ -8,24 +9,24 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\KategoriController;
 
+Route::get('/', [WelcomeController::class, 'index']);
 
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-Route::put('/user/hapus_simpan/{id}', [UserController::class, 'hapus_simpan']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::put('/user/hapus_simpan/{id}', [UserController::class, 'hapus_simpan']);
 
-Route::get('/', function () {
-    return "Welcome Page";
-});
+// Route::get('/', function () {
+//     return "Welcome Page";
+// });
 
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [userController::class, 'index']);
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [userController::class, 'index']);
 // Route::get('/', function(){
 //     return view('welcome');
 // });
